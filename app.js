19e03ae5bee1deb9e83731c44e22657093338efe
Sasha <kremlink0@gmail.com>
2022-01-16 00:33:43 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const a = require('./modules/one.js')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!'+a.a+2)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
